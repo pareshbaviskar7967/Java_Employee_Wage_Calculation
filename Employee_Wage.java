8173e83b6ad54fafc21e0ea508883c39500533ae
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-24 11:31:12 +0530

    UC-1 Check Employee is

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,5 +1,11 @@
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Hello....Welcome to the Employee Wage Computation Problems");
+
+        int CheckEmp = (int) Math.floor(Math.random() * 10 % 2);
+        if (CheckEmp == 0)
+            System.out.println("Employee is not present");
+        else
+            System.out.println("Employee is present");
     }
 }
