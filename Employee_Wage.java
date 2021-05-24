b04071578908ee554f74ac257a5ca5fb9361c339
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-24 17:18:24 +0530

    UC-4 solving using switch-case

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,20 +1,26 @@
 public class Employee_Wage {
+
+    public static final int Emp_RatePerHrs = 20;
+
     public static void main(String[] args) {
 
-        int Emp_RatePerHrs = 20;
         int emphrs = 0;
         int empwage = 0;
         int CheckEmp = (int) Math.floor(Math.random() * 10 % 3);
 
-        if (CheckEmp == 1) {
-            System.out.println("Employee is present full-time");
-            emphrs = 8;
-        } else if (CheckEmp == 2) {
-            System.out.println("Employee is present part-time");
-            emphrs = 4;
-        } else {
-            System.out.println("Employee is absent");
-            emphrs = 0;
+        switch (CheckEmp) {
+            case 1:
+                System.out.println("Employee is present full-time");
+                emphrs = 8;
+                break;
+            case 2:
+                System.out.println("Employee is present part-time");
+                emphrs = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                emphrs = 0;
+                break;
         }
 
         empwage = emphrs * Emp_RatePerHrs;
