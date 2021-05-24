70c57261d705ab1b41529f303e7b5bc42d7e31e5
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-24 11:51:07 +0530

    UC-2 Calculate daily employee wage(PerHrs20 & FullDayHrs8)

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,11 +1,18 @@
 public class Employee_Wage {
     public static void main(String[] args) {
-        System.out.println("Hello....Welcome to the Employee Wage Computation Problems");
 
+        int Emp_RatePerHrs = 20;
+        int emphrs = 0;
+        int empwage = 0;
         int CheckEmp = (int) Math.floor(Math.random() * 10 % 2);
-        if (CheckEmp == 0)
-            System.out.println("Employee is not present");
-        else
-            System.out.println("Employee is present");
+
+        if (CheckEmp == 1) {
+            emphrs = 8;
+        } else {
+            emphrs = 0;
+        }
+
+        empwage = emphrs * Emp_RatePerHrs;
+        System.out.println("Employee Wage is : " + empwage);
     }
 }
